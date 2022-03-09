e2df0906ce47b88228057b05bc2e75623149be26
reelcollins <72294833+reelcollins@users.noreply.github.com>
2022-03-09 12:14:54 +0300

    Create array3.c

---
+++ array3.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv[]) {
+int i;
+for(i = 0; i<argv; i++){
+    printf("argument no.: %d\t %s\n",i,argv[i]);
+   
+}
+
+}
